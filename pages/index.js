86266e9e48d9e553147f064f52c06c89c6a39696
Logan Zehr <zehrl315@outlook.com>
2023-03-24 21:11:35 -0400

    Basic pawn movement with dragging

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,14 @@ export default function Home() {
     <>
       <main>
         <div id='chess-board'>
-          <File file="8" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="7" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="6" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="5" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="4" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="3" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="2" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
-          <File file="1" pieceLocation={`${pieceLocation.rank}${pieceLocation.file}`}><Pawn /></File>
+          <File file="8" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="7" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="6" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="5" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="4" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="3" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="2" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
+          <File file="1" pieceLocation={pieceLocation} setPieceLocation={setPieceLocation}><Pawn /></File>
         </div>
       </main>
     </>
@@ -27,28 +27,54 @@ export default function Home() {
 function File(props) {
   const piece = props.children;
   const pieceLocation = props.pieceLocation;
+  const setPieceLocation = props.setPieceLocation
+
+  const handleDragOver = (event) => {
+    event.preventDefault()
+  }
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const id = event.target.id;
+
+    if(!!id){
+      const rank = id.split("")[0];
+      const file = id.split("")[1];
+      setPieceLocation({rank: rank, file: file});
+    }
+  }
+
   return (
     <div className='file'>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`A${props.file}`}>{pieceLocation === `A${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`B${props.file}`}>{pieceLocation === `B${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`C${props.file}`}>{pieceLocation === `C${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`D${props.file}`}>{pieceLocation === `D${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`E${props.file}`}>{pieceLocation === `E${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`F${props.file}`}>{pieceLocation === `F${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`G${props.file}`}>{pieceLocation === `G${props.file}` && piece}</div>
-      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`H${props.file}`}>{pieceLocation === `H${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`A${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `A${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`B${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `B${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`C${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `C${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`D${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `D${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`E${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `E${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`F${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `F${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`G${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `G${props.file}` && piece}</div>
+      <div className='square' onDragOver={handleDragOver} onDrop={handleDrop} id={`H${props.file}`}>{`${pieceLocation.rank}${pieceLocation.file}` === `H${props.file}` && piece}</div>
     </div>
   )
 }
 
 function Pawn() {
-  isGrabbed = false;
+  
+  const handleDragEnd = (event) => {
+  }
+
+  const handleDragStart = (event) => {
+    event.dataTransfer.effectAllowed = "move";
+  }
 
   return (
     <Image 
-      className={`piece${isGrabbed ? " grabbing" : " grab"}`}
+      className={`piece grab`}
       src={whitePawn} 
       draggable
+      onDragEnd = {handleDragEnd}
+      onDragStart = {handleDragStart}
+      alt = "white pawn"
     />
   )
 }
